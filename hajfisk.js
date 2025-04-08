f8df2c16f440327737ded2a582e6b7a0944ff5e5
Dyreborg <augustdyreborg@gmail.com>
2025-04-08 09:39:42 +0200

    hajfisk

---
+++ hajfisk.js
@@ -0,0 +1,11 @@
+class hajFisk{
+    #pos; #has; #x; #y
+        this.#pos = pos;
+        this.#has = has;
+        this.#x = 60;
+        this.#y = 30;
+}
+
+update(){
+    this.#pos=this.#pos + this.#has
+}
